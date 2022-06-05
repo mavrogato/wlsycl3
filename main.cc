65dba938406baefe361db27d2f70e33c3ce6a6ac
Nakashima, Terumi <nakashima.terumi@gmail.com>
2022-06-05 15:39:35 +0900

    Test

--- main.cc
+++ main.cc
@@ -4,7 +4,7 @@
 
 #include <utility>
 
-// >>>
+// >>> libstdc++ coroutine for clang
 #define __cpp_impl_coroutine 1
 # include <coroutine>
 #undef  __cpp_impl_coroutine
@@ -13,7 +13,7 @@ namespace std::inline experimental
     using std::coroutine_traits;
     using std::coroutine_handle;
 }
-// <<< dirty hack for using libstdc++ coroutine (not libc++) for clang++
+// <<<
 
 #endif/*INCLUDE_COROUTINE_HACK_HPP_87B5F3B0_F8BE_49D4_A4B3_2783FF063BD4*/
 
@@ -23,66 +23,70 @@ namespace std::inline experimental
 
 #include <tuple>
 #include <iosfwd>
-//#include "coroutine-hack.hpp"
+#include <cstdint>
 
 namespace aux
 {
-    template <std::size_t...> struct seq { };
-    template <std::size_t N, std::size_t... I> struct gen_seq : gen_seq<N-1, N-1, I...> { };
-    template <std::size_t... I> struct gen_seq<0, I...> : seq<I...> { };
-    template <class Ch, class T, std::size_t... I>
-    void print(std::basic_ostream<Ch>& output, T const& t, seq<I...>) noexcept {
-        using swallow = int[];
-        (void) swallow{0, (void(output << (I==0 ? "" : ", ") << std::get<I>(t)), 0)...};
-    }
-    template <class Ch, class... Args>
-    auto& operator<<(std::basic_ostream<Ch>& output, std::tuple<Args...> const& t) noexcept {
-        output << '(';
-        print(output, t, gen_seq<sizeof...(Args)>());
-        output << ')';
-        return output;
-    }
-
-    template <class T>
-    struct filament {
-        struct promise_type {
-            void unhandled_exception() { throw; }
-            auto get_return_object() noexcept { return filament{*this}; }
-            auto initial_suspend() noexcept { return std::suspend_always{}; }
-            auto final_suspend() noexcept { return std::suspend_always{}; }
-            auto yield_value(T value) noexcept {
-                this->result = value;
-                return std::suspend_always{};
+    inline namespace tuple
+    {
+        template <size_t...> struct seq { };
+        template <size_t N, size_t... I> struct gen_seq : gen_seq<N-1, N-1, I...> { };
+        template <size_t... I> struct gen_seq<0, I...> : seq<I...> { };
+        template <class Ch, class T, size_t... I>
+        void print(std::basic_ostream<Ch>& output, T const& t, seq<I...>) noexcept {
+            using swallow = int[];
+            (void) swallow{0, (void(output << (I==0 ? "" : ", ") << std::get<I>(t)), 0)...};
+        }
+        template <class Ch, class... Args>
+        auto& operator<<(std::basic_ostream<Ch>& output, std::tuple<Args...> const& t) noexcept {
+            output << '(';
+            print(output, t, gen_seq<sizeof...(Args)>());
+            output << ')';
+            return output;
+        }
+    } // ::aux::tuple
+    inline namespace coroutine
+    {
+        template <class T>
+        struct filament {
+            struct promise_type {
+                void unhandled_exception() { throw; }
+                auto get_return_object() noexcept { return filament{*this}; }
+                auto initial_suspend() noexcept { return std::suspend_always{}; }
+                auto final_suspend() noexcept { return std::suspend_always{}; }
+                auto yield_value(T value) noexcept {
+                    this->result = value;
+                    return std::suspend_always{};
+                }
+                auto return_void() noexcept { }
+                T result;
+            };
+            filament() = delete;
+            filament(filament const&) = delete;
+            filament(filament&& src)
+                : handle{std::exchange(src.handle, nullptr)}
+            {
             }
-            auto return_void() noexcept { }
-            T result;
+            filament& operator=(filament const&) = delete;
+            filament& operator=(filament&& src) {
+                if (this != &src) {
+                    this->handle = std::exchange(src.handle, nullptr);
+                }
+                return *this;
+            }
+            ~filament() noexcept { if (this->handle) this->handle.destroy(); }
+            T step() const noexcept {
+                this->handle.resume();
+                return this->handle.promise().result;
+            }
+        private:
+            explicit filament(promise_type& p) noexcept
+                : handle{std::coroutine_handle<promise_type>::from_promise(p)}
+            {
+            }
+            std::coroutine_handle<promise_type> handle;
         };
-        filament() = delete;
-        filament(filament const&) = delete;
-        filament(filament&& src)
-            : handle{std::exchange(src.handle, nullptr)}
-        {
-        }
-        filament& operator=(filament const&) = delete;
-        filament& operator=(filament&& src) {
-            if (this != &src) {
-                this->handle = std::exchange(src.handle, nullptr);
-            }
-            return *this;
-        }
-        ~filament() noexcept { if (this->handle) this->handle.destroy(); }
-        T step() const noexcept {
-            this->handle.resume();
-            return this->handle.promise().result;
-        }
-
-    private:
-        explicit filament(promise_type& p) noexcept
-            : handle{std::coroutine_handle<promise_type>::from_promise(p)}
-        {
-        }
-        std::coroutine_handle<promise_type> handle;
-    };
+    } // ::aux::coroutine
 } // ::aux
 
 #endif/*INCLUDE_AUXILIARY_HPP_C0F6A1AF_FB18_4AA4_BAF9_6378F166F394*/
@@ -97,7 +101,7 @@ namespace aux
 #include <string_view>
 #include <wayland-client.h>
 
-inline namespace wayland_client_helper
+namespace wayland_client_helper
 {
     template <class> constexpr std::nullptr_t wl_interface_ptr = nullptr;
 #define INTERN_WL_INTERFACE(wl_client)                                  \
@@ -152,36 +156,56 @@ inline namespace wayland_client_helper
 
     template <WL_CLIENT_CONCEPT T>
     using wl_client_ptr = decltype (attach_unique(std::declval<T>()));
-}
+
+} // ::wayland_client_helper
 
 #endif/*INCLUDE_WAYLAND_CLIENT_HELPER_HPP_5EB58011_CE66_477C_BA2C_00259E712521*/
 
 /////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <thread>
+#include <map>
+#include <string>
 
 int main() {
     using namespace std::literals;
     using namespace aux;
     using namespace wayland_client_helper;
 
-    constexpr static auto process = [](auto... args) noexcept {
-        std::cout << std::tuple{args...} << std::endl;
-    };
+    std::map<uint32_t, std::pair<std::string, uint32_t>> globals;
 
-    if (auto display = attach_unique(wl_display_connect(nullptr))) {
-        if (auto registry = attach_unique(wl_display_get_registry(display.get()))) {
-            static constexpr wl_registry_listener listener {
-                .global = [](auto... args) noexcept { process(args...); },
-                .global_remove = [](auto... args) noexcept { process(args...); },
-            };
-            if (auto ret = wl_registry_add_listener(registry.get(), &listener, nullptr); ret == 0) {
-                for (;;) {
-                    wl_display_roundtrip(display.get());
-                    std::this_thread::sleep_for(1s);
-                }
-            }
-        }
+    auto display = attach_unique(wl_display_connect(nullptr));
+    if (!display) {
+        std::cerr << "wl_display_connect failed..." << std::endl;
+        return -1;
+    }
+    auto registry = attach_unique(wl_display_get_registry(display.get()));
+    if (!registry) {
+        std::cerr << "wl_display_get_registry failed..." << std::endl;
+        return -1;
+    }
+    wl_registry_listener registry_listener = {
+        .global = [](void* data, auto... args) noexcept {
+            std::cerr << std::tuple{args...} << std::endl;
+            auto globals_ptr = reinterpret_cast<decltype (globals)*>(data);
+            auto [wl_registry, name, interface, version] = std::tuple{args...};
+            globals_ptr->insert({name, {std::string(interface), version}});
+        },
+        .global_remove = [](void* data, auto... args) noexcept {
+            std::cerr << std::tuple{args...} << std::endl;
+            auto globals_ptr = reinterpret_cast<decltype (globals)*>(data);
+            auto [wl_registry, name] = std::tuple{args...};
+            globals_ptr->erase(name);
+        },
+    };
+    if (wl_registry_add_listener(registry.get(), &registry_listener, &globals) != 0) {
+        std::cerr << "wl_registry_add_listener failed..." << std::endl;
+        return -1;
+    }
+    for (;;) {
+        wl_display_roundtrip(display.get());
+        std::this_thread::sleep_for(1s);
+        std::cout << globals.size() << std::endl;
     }
 
     return 0;
